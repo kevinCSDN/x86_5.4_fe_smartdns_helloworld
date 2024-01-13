55a8032fea1b906142a0f9e80b0a8a36bff2e1f1
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-01-13 14:40:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,9 +20,10 @@ git clone https://github.com/haiibo/openwrt-packages
 #赋予删除的权限         
 shopt -s extglob
 #删除除了luci-app-bypass之外的所有插件，如果保留smartdns可以这样写rm -rf openwrt-packages/!(luci-app-bypass|luci-app-smartdns)
-rm -rf openwrt-packages/!(luci-app-openclash)
+rm -rf openwrt-packages/!(luci-app-vssr|lua-maxminddb)
 #拷贝到插件库package
-cp -r openwrt-packages/luci-app-openclash package/
+cp -r openwrt-packages/luci-app-vssr package/
+cp -r openwrt-packages/lua-maxminddb package/
 rm -rf openwrt-packages
 
 #删除feeds自带mosdns、v2ray-geodata
