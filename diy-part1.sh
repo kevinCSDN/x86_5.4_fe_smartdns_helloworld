cfa3128e414a1ba8645373fd643a48f138a74ac7
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-01-15 07:55:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+#git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 
 #拉取文件包里一个插件的方法
 #拉取整个包
